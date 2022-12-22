13252dc9380d8da46ce22c2c1ab1b468bda4d536
Valera-Filimonov <68681899+Valera-Filimonov@users.noreply.github.com>
2022-12-22 12:56:41 +0300

    feat: released app on js

---
+++ src/components/Menu.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../App.css";
+
+const Menu = ({setLineColor, setLineWidth, setLineOpacity}) => {
+
+    const onChangeHandlerColor = (event) => {
+        setLineColor(event.target.value);
+    }
+
+    const onChangeHandlerWidth = (event) => {
+        setLineWidth(event.target.value);
+    }
+
+    const onChangeHandlerTransparency = (event) => {
+        setLineOpacity(event.target.value / 100);
+    }
+
+    return (
+        <div className="Menu">
+            <label>Цвет кисти</label>
+            <input
+                type="color"
+                onChange={onChangeHandlerColor}
+            />
+            <label>Ширина кисти</label>
+            <input
+                type="range"
+                min="3"
+                max="20"
+                onChange={onChangeHandlerWidth}
+            />
+            <label>Прозрачность кисти</label>
+            <input
+                type="range"
+                min="1"
+                max="100"
+                onChange={onChangeHandlerTransparency}
+            />
+        </div>
+    );
+};
+
+export default Menu;
